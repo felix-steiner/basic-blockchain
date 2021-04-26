54d367d11d01dd9d9fae27877f04bdef664ca797
Felix Steiner <felix.steiner7@gmail.com>
2021-04-26 14:58:10 +0200

    Added an example usage of the blockchain

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello World');
+import { Chain } from "./chain";
+import { Wallet } from "./wallet";
+
+const satoshi = new Wallet();
+const bob = new Wallet();
+const alice = new Wallet();
+
+satoshi.send(50, bob.publicKey);
+bob.send(23, alice.publicKey);
+alice.send(5, bob.publicKey);
+
+console.log(Chain.instance);
